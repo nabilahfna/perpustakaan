9327932b9e31e4dfcb1d358cfc3532ce75426a8f
nabilahfna <93857985+nabilahfna@users.noreply.github.com>
2021-11-07 19:13:45 +0700

    Update transaksi-edit-pengembalian.php

--- pages/transaksi-edit-pengembalian.php
+++ pages/transaksi-edit-pengembalian.php
@@ -16,30 +16,7 @@
 <div id="label-page"><h3>Edit Data Transaksi</h3></div>
 <div id="content">
 	<form action="proses/transaksi-edit-proses.php" method="post" enctype="multipart/form-data">
-	<table id="tabel-input">
-		<tr>
-			<td class="label-formulir">ID Transaksi</td>
-			<td class="isian-formulir"><input type="text" name="idtransaksi" value="<?php echo $r_tampil_transaksi['idtransaksi']; ?>" readonly="readonly" class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">ID Anggota</td>
-			<td class="isian-formulir"><input type="text" name="idanggota" value="<?php echo $r_tampil_transaksi['idanggota']; ?>" readonly="readonly" class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">ID Buku</td>
-			<td class="isian-formulir"><input type="text" name="idbuku" value="<?php echo $r_tampil_transaksi['idbuku']; ?>" readonly="readonly" class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Tanggal Pinjam</td>
-			<td class="isian-formulir"><input type="date" name="tglpinjam" value="<?php echo $r_tampil_transaksi['tglpinjam']; ?>" readonly="readonly" class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>>
-		</tr>
-		<tr>
-			<td class="label-formulir">Tanggal Kembali</td>
-			<td class="isian-formulir"><input type="date" name="tglkembali" value="<?php echo $r_tampil_transaksi['tglkembali']; ?>"  class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>>
-		</tr>
-		<tr>
-			<td class="label-formulir"></td>
-			<td class="isian-formulir"><input type="submit" name="simpan" value="Simpan" id="tombol-simpan"></td>
-		</tr>
+	
+		
 		
 		
